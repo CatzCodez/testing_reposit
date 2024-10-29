dae346ef5ba544f40d1c58ad1952548389939037
Brian Nguyen <briannguyenmain@gmail.com>
2024-10-29 18:38:17 -0400

    making changes for vinnie to see

--- testing.py
+++ testing.py
@@ -8,3 +8,9 @@ class Person:
         connections (set of Person): other people in the social network
                                      who know this person.
     """
+    
+    def __init__(self, name):
+        """Initialize a new Person object."""
+        
+        self.name = name
+        self.connections = set()
